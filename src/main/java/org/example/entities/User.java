269a81896422b95902baae3b1de7795eb3439653
Kate-Kn <kateryna.kondtaiuk@ukma.edu.ua>
2021-06-11 09:44:37 +0300

    changed project structure

---
+++ src/main/java/org/example/entities/User.java
@@ -0,0 +1,24 @@
+package org.example.entities;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String login;
+    private String password;
+    private int id_user;
+    private String role;
+    public User(String login, String password, String role)
+    {
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+    public User(int id, String login, String password, String role)
+    {
+        this.id_user = id;
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+}
